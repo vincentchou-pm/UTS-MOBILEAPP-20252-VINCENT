c31911348846d7f67a3b23aca4590244cc9cdd30
vincentchou-pm <23502410025@student.prasetiyamulya.ac.id>
2026-02-24 10:37:40 +0700

    mockup and model

---
+++ simple_marketplace/lib/mockup/mockup_products.dart
@@ -0,0 +1,7 @@
+import '../model/products_model.dart';
+
+List<Product> mockProducts = [
+  Product(name: 'Phone', description: 'Comfortable colors', price: 99),
+  Product(name: 'T-Shirt', description: 'Soft cotton', price: 19.99),
+  Product(name: 'Headphones', description: 'Great sounding', price: 49),
+];
